f795cf014a3401be0125f1f279f8c8879a64cb93
Abigail Bergman <abbygirlrose@gmail.com>
2018-12-05 15:41:32 -0600

    working on UIOutput

--- olympic_medals/app.R
+++ olympic_medals/app.R
@@ -4,6 +4,9 @@ library(tidyverse)
 library(readr)
 library(ggplot2)
 library(dplyr)
+library(shinythemes)
+library(tidyr)
+library(rsconnect)
 summer <- read_csv("../summer.csv")%>%
   mutate(Season= "Summer")
 winter <- read_csv("../winter.csv")%>%
@@ -11,59 +14,32 @@ winter <- read_csv("../winter.csv")%>%
 dictionary <- read_csv("../dictionary.csv")
 
 olympic <- bind_rows(summer, winter)%>%
-  select(Year, Sport, Country, Gender, Medal, Season)
-
-
-medal_country_year <- olympic%>%
-  count(Year, Country, Medal)
-
-medal_gender_year <- olympic%>%
-  count(Year, Gender, Medal)
-
-med <- medal_gender_year %>%
-  group_by(Medal)%>%
-ggplot(aes(Year, n, color = Medal, fill = Medal)) +
-  geom_col() +
-  labs(y = "Number of Medals") +
-  scale_color_manual(values=c("#cd7f32", "#E69F00", "#999999")) + #make colors Bronze, Silver, Gold
-  scale_fill_manual(values=c( "#cd7f32","#E69F00",  "#999999")) 
-
-country <- medal_country_year %>%
-  ggplot(aes(Country, Year, color = Medal, fill = Medal)) +
-  geom_col() +
-  labs(y = "Number of Medals") +
-  scale_color_manual(values=c("#cd7f32", "#E69F00", "#999999")) + #make colors Bronze, Silver, Gold
-  scale_fill_manual(values=c( "#cd7f32","#E69F00","#999999")) 
- 
+  select(Year, Sport, Country, Gender, Medal, Season) 
 
 #https://www.kaggle.com/the-guardian/olympic-games
 col <- c(colnames(olympic))
 
-ui <- fluidPage(h1("Olympic Medals"),
+ui <- fluidPage(theme = shinytheme("simplex"),
+  h1("Olympic Medals"),
                 sidebarLayout(
                   sidebarPanel(
-                    selectInput(inputId = "Filter",
-                                label = "Select Filters",
-                                choices = col,
-                                multiple = TRUE),
-                sliderInput(inputId = "Year", 
+                    sliderInput(inputId = "Year", 
                             label = "Select Year",
                             min = min(olympic$Year),
                             max = max(olympic$Year),
                             value = c(min(olympic$Year),max(olympic$Year)),
                             sep = ""), #got rid of 1000s separator
-                selectInput("Sport", "Select Sport",
-                            choices = sort(unique(olympic$Sport)),
-                            multiple = FALSE),
                 selectInput("Country", "Select Country",
                             choices = sort(unique(olympic$Country)),
                             multiple = FALSE),
                 radioButtons("Gender", "Select Gender",
                              choices = sort(unique(olympic$Gender))),
                 radioButtons("Season", "Select Season",
-                             choices = sort(unique(olympic$Season)))
+                             choices = sort(unique(olympic$Season))),
+                uiOutput("sportSelector")
                 ),
-                mainPanel(plotOutput("medalsPlot"))))
+                mainPanel(plotOutput("medalsPlot"),
+                          tableOutput("medalsTable"))))
 server <- function(input, output) {
   medal_filter <- reactive({
     olympic %>%
@@ -75,16 +51,44 @@ server <- function(input, output) {
         Year <= input$Year[[2]], 
         #filter by country
         Country == input$Country,
-        Gender == input$Gender
+        Gender == input$Gender,
+        Sport == input$Sport
       )    
   })
+  
   output$medalsPlot <- renderPlot({
       ggplot(medal_filter(), aes(Year, color = Medal, fill = Medal)) +
       geom_histogram()+
       labs(y = "Number of Medals") +
       scale_color_manual(values=c("#cd7f32", "#E69F00", "#999999")) + #make colors Bronze, Silver, Gold
       scale_fill_manual(values=c( "#cd7f32","#E69F00",  "#999999"))
+    })
+  output$medalsTable <- renderTable({
+    medal_filter()%>%
+      count(Medal, Year, Sport, Gender)%>%
+      mutate("Number of Medals" = n) %>%
+      select(-n) %>%
+      spread(Medal, `Number of Medals`)
+  }) 
+  output$sportSelector <- renderUI({
+    medals <- olympic
+    
+    # filter by country
+    if(!is.null(input$Country)) {
+      medals <- filter(medals, Country %in% input$Country)
+    }
+    medals <- filter(medals, Gender == input$Gender)
+    
+    medals <- filter(medals, Season == input$Season)
+    
+    # filter by year
+    medals <- filter(medals,
+                     Year >= input$Year[[1]],
+                     Year <= input$Year[[2]])
+    selectInput("Sport", "Select Sport",
+                choices = sort(unique(medals$Sport)),
+                multiple = FALSE)
   })
-  
+    
 }
 shinyApp(ui = ui, server = server)
